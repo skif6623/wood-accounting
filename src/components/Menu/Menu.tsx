10b687a11dc21440fd7419cbe4efd483ea72e7c9
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-08-10 00:01:39 +0300

    add closing menu on press escape

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useCallback } from 'react';
 import { MenuLayout, Navigation, Link, CloseIcon } from './Menu.styled';
 
 import type { HeaderTypes } from '../Header/Header';
 
 export const Menu: FC<HeaderTypes> = ({ isOpen, toggle }) => {
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        toggle();
+      }
+    },
+    [toggle]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown);
+    }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, handleKeyDown]);
+
+  const handleClick = (e: any) => {
+    const targetMenu = e.target;
+    if (e.target !== targetMenu) {
+      console.log('закрито');
+    } else {
+      console.log('відкрито');
+    }
+  };
+
   return (
-    <MenuLayout open={isOpen}>
+    <MenuLayout open={isOpen} onClick={handleClick}>
       <Navigation>
         <Link to="/">Склад</Link>
         <Link to="/add">Приход кругляку</Link>
