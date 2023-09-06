14e95ecaa378a85324cb2f9a96878c13c7418e9b
ImVladislav <nafanya102@gmail.com>
2023-09-06 21:29:26 +0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,8 @@ import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { WoodStorage } from '../Pages/WoodStorage/WoodStorage';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { RoundWoodList } from './RoundWood/RoundWoodList/RoundWoodList';
-// import { BoardList } from './BoardList/BoardList';
-// import { ChooseWoodType } from '../Pages/ChooseWoodType/ChooseWoodType';
+import { BoardList } from './Boards/BoardList/BoardList';
+import { ChooseWoodType } from '../Pages/ChooseWoodType/ChooseWoodType';
 import { AddItemsForm } from '../Pages/AddItems/AddItems';
 import { RegisterPage } from '../Pages/RegisterPage/RegisterPage';
 import { LoginPage } from '../Pages/LoginPage/LoginPage';
@@ -31,43 +31,47 @@ export const App: React.FC = () => {
   
   
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-              <Route index element={<WoodStorage />} />
-              <Route path="wood" element={<RoundWoodList />} />
-              <Route path="board-wood" element={<AddItemsForm />} />
-        </Route>
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+//               <Route index element={<WoodStorage />} />
+//               <Route path="wood" element={<RoundWoodList />} />
+//               <Route path="board-wood" element={<AddItemsForm />} />
+//         </Route>
 
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<p>тут ніхуя нема скільки не дивись</p>} />
-      </Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/register" element={<RegisterPage />} />
+//         <Route path="*" element={<p>тут ніхуя нема скільки не дивись</p>} />
+//       </Routes>
 
       
-    </div>
-  );
+//     </div>
+//   );
+// };
+
+
+    <div>
+    {/* <Routes>
+      {!isLoggedIn && <Route index element={<LoginPage />} />}
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes> */}
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/" element={<WoodStorage />}>
+          <Route index element={<RoundWoodList />} />
+          <Route path="board" element={<BoardList />} />
+          <Route path="pallets" element={<h1>Піддони</h1>} />
+        </Route>
+        <Route path="/choose-wood" element={<ChooseWoodType />} />
+        <Route path="/round-wood" element={<AddItemsForm />} />
+        <Route path="/board-wood" element={<AddItemsForm />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+      </div>
+      );
 };
-
-
-    // <div>
-    // <Routes>
-    //   {/* {!isLoggedIn && <Route index element={<LoginPage />} />} */}
-    //   <Route path="/" element={<SharedLayout />}>
-    //     <Route index element={<RegisterPage />} />
-    //     <Route path="/login" element={<LoginPage />} />
-    //   </Route>
-    // </Routes>
-    // <Routes>
-    //   <Route path="/" element={<SharedLayout />}>
-    //     <Route path="/" element={<WoodStorage />}>
-    //       <Route index element={<RoundWoodList />} />
-    //       <Route path="board" element={<BoardList />} />
-    //       <Route path="pallets" element={<h1>Піддони</h1>} />
-    //     </Route>
-    //     <Route path="/choose-wood" element={<ChooseWoodType />} />
-    //     <Route path="/round-wood" element={<AddItemsForm />} />
-    //     <Route path="/board-wood" element={<AddItemsForm />} />
-    //   </Route>
-    //   </Routes>
-    //   </div>
